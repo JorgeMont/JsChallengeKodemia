d0494dd8e0d54cf4109ec1d89e41a1999114e710
JorgeMont <monterrosaspix@gmail.com>
2023-01-26 11:29:21 -0600

    Adding tags list

--- JS/Helpers/createCard.js
+++ JS/Helpers/createCard.js
@@ -1,3 +1,11 @@
+const createLiTag = (tagText) => {
+    const liTag = document.createElement('li');
+    const anchorTag = document.createElement('a');
+    anchorTag.textContent = `#${tagText}`;
+    liTag.appendChild(anchorTag);
+    return liTag;
+}
+
 const createCard = (cardData) => {
 
     //console.log(cardData);
@@ -15,9 +23,6 @@ const createCard = (cardData) => {
     const pDate = document.createElement('p');
     const hTitle = document.createElement('h3');
     const tagList = document.createElement('ul');
-    //Reusable
-    const liTag = document.createElement('li');
-    const anchorTag = document.createElement('a');
     const divContainerBtns = document.createElement('div');
     const divCntLikeComment = document.createElement('div');
     const divCntReactions = document.createElement('div');
@@ -48,16 +53,17 @@ const createCard = (cardData) => {
     divContainerNameDate.appendChild(pName);
     pName.classList.add('fw-bold');
     divContainerNameDate.appendChild(pDate);
-    pDate.textContent = cardData.date;
+    pDate.textContent = `Posted on ${cardData.date}`;
     pDate.classList.add('time');
     divCardBody.appendChild(hTitle);
     hTitle.classList.add('card-title', 'ms-4');
     divCardBody.appendChild(tagList);
     tagList.classList.add('d-flex', 'flex-wrap', 'p-0' ,'ms-4');
-    tagList.appendChild(anchorTag)
+    // Inserting tags
     cardData.tags.forEach(tag => {
-        anchorTag.appendChild(liTag);
-        liTag.textContent = tag;
+        const newli = createLiTag(tag);
+        console.log(newli);
+        tagList.appendChild(newli);
     });
     divCardBody.appendChild(divContainerBtns);
     divContainerBtns.classList.add('card__buttons', 'd-flex', 'ms-4');
@@ -112,26 +118,21 @@ const createCard = (cardData) => {
     // anchor.appendChild(prueba);
 
     // Add styles to elements
-    // card.classList.add('mentorCard');
-    // card.classList.add('card', 'm-3');
-    // imgCover.classList.add('card-img-top');
-    // divCardBody.classList.add('card-body');
-    // divCardUserDetails.classList.add('card__userDetails', 'd-flex', 'align-items-center', 'mb-2');
-    // divContainerNameDate.classList.add('d-flex', 'flex-column');
-    // pName.classList.add('fw-bold');
-    // pDate.classList.add('time');
-    // hTitle.classList.add('card-title', 'ms-4');
-    // tagList.classList.add('d-flex', 'flex-wrap', 'p-0' ,'ms-4');
-    // divContainerBtns.classList.add('card__buttons', 'd-flex', 'ms-4');
-    // divCntLikeComment.classList.add('interactions', 'd-flex');
-    // divCntReactions.classList.add('reactions', 'd-flex', 'me-4');
-    // divCntComments.classList.add('comments', 'd-flex');
-    // divCntSave.classList.add('tools', 'd-flex');
-
-    //Add content to DOM elements
-    // imgCover.src = cardData.coverImage;
-    // pDate.textContent = `Posted on ${cardData.date}`;
-    // hTitle.textContent = cardData.titulo;
+    card.classList.add('mentorCard');
+    card.classList.add('card', 'm-3');
+    imgCover.classList.add('card-img-top');
+    divCardBody.classList.add('card-body');
+    divCardUserDetails.classList.add('card__userDetails', 'd-flex', 'align-items-center', 'mb-2');
+    divContainerNameDate.classList.add('d-flex', 'flex-column');
+    pName.classList.add('fw-bold');
+    pDate.classList.add('time');
+    hTitle.classList.add('card-title', 'ms-4');
+    tagList.classList.add('d-flex', 'flex-wrap', 'p-0' ,'ms-4');
+    divContainerBtns.classList.add('card__buttons', 'd-flex', 'ms-4');
+    divCntLikeComment.classList.add('interactions', 'd-flex');
+    divCntReactions.classList.add('reactions', 'd-flex', 'me-4');
+    divCntComments.classList.add('comments', 'd-flex');
+    divCntSave.classList.add('tools', 'd-flex');
     
     return anchor;
 }
